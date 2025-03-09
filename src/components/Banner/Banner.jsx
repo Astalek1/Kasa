5f43a1ac3c622b9fc74d747185cf974dc70f8a64
kévin goujon <goujon.kevin@hotmail.fr>
2025-03-09 17:36:03 +0100

    ajout du composant Banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,1 +1,12 @@
+import './Banner.scss'
 
+function Banner({ image, title }) {
+  return (
+    <div className="banner">
+      <img src={image} alt="img-banner" />
+      {title && <h1>{title}</h1>}
+    </div>
+  )
+}
+
+export default Banner
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,16 @@
 //import Logements from 'src/data/data.json'
-
 import './Home.scss'
+import Banner from 'src/components/Banner/Banner.jsx'
+import homeBannerImage from 'src/assets/Image-source-1.svg'
 
 function Home() {
   return (
     <>
       <div className="Home">
-        <h1>Page Home</h1>
+        <Banner
+          image={homeBannerImage}
+          title="Chez vous, partout et ailleurs"
+        />
       </div>
     </>
   )
