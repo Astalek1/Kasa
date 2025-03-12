95ebf33af47f71fda670c2671ff725aeddff84ab
kévin goujon <goujon.kevin@hotmail.fr>
2025-03-12 10:46:42 +0100

    modificartion des cards et ajout du recangle

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,23 +1,17 @@
 import PropTypes from 'prop-types'
 import './Cards.scss'
 
-function HousingCard({ id, title, cover }) {
+function HousingCard({ title, cover }) {
   return (
-    <>
-      <div
-        key={id}
-        id={id}
-        className="housing-card"
-        style={{ backgroundImage: `url(${cover})` }}
-      >
-        <h2 className="housing-title">{title}</h2>
-      </div>
-    </>
+    <article className="housing-card">
+      <img src={cover} alt={title} className="housing-img" />
+      <div className="housing-rectangle" />
+      <h2 className="housing-title">{title}</h2>
+    </article>
   )
 }
 
 HousingCard.propTypes = {
-  id: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   cover: PropTypes.string.isRequired,
 }
