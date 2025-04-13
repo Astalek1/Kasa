3af54ee10dd367ecbcc3a394b3ede6e8bbd5cca3
kévin goujon <goujon.kevin@hotmail.fr>
2025-04-13 15:21:42 +0200

    modifications sass

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -3,21 +3,19 @@ import { Link } from 'react-router-dom'
 
 function Error() {
   return (
-    <>
-      <main>
-        <div className="Error">
-          <p className="Error__404">404</p>
-          <p className="Error__txt">
-            Oups! la page que
-            <br className="Error__responsive--break" /> vous demandez n'existe
-            pas.
-          </p>
-        </div>
-        <Link className="Error__link" to="/">
-          <p className="Error__link--txt">Retourner sur la page d'accueil</p>
-        </Link>
-      </main>
-    </>
+    <main>
+      <div className="Error">
+        <p className="Error__404">404</p>
+        <p className="Error__txt">
+          Oups! la page que
+          <br className="Error__responsive--break" /> vous demandez n'existe
+          pas.
+        </p>
+      </div>
+      <Link className="Error__link" to="/">
+        <p className="Error__link--txt">Retourner sur la page d'accueil</p>
+      </Link>
+    </main>
   )
 }
 
